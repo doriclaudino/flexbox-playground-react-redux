a2cfd34c2e2441123031209056d9eebbf623412a
dori claudino <dori.claudino@gmail.com>
2018-06-11 14:50:13 -0400

    inline style

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,60 +1,60 @@
-import { createStore } from "redux";
-import { editorReducer } from "./reducers";
-import v4 from "uuid";
-import randomColor from "randomcolor";
+  import { createStore } from "redux";
+  import { editorReducer } from "./reducers";
+  import v4 from "uuid";
+  import randomColor from "randomcolor";
 
-const configureStore = () => {
-  const persistedState = {
-    base_child: {
-      id: v4(),
-      className: "flex half-size hover-effect",
-      style: { backgroundColor: randomColor() },
-      items: []
-    },
-    items: [
-      {
+  const configureStore = () => {
+    const persistedState = {
+      base_child: {
         id: v4(),
-        className: "flex half-size hover-effect",
-        style: { backgroundColor: randomColor() },
-        items: [
-          {
-            id: v4(),
-            className: "flex half-size hover-effect",
-            style: { backgroundColor: randomColor() },
-            items: [
-              {
-                id: v4(),
-                className: "flex half-size hover-effect",
-                style: { backgroundColor: randomColor() },
-                items: []
-              }
-            ]
-          }
-        ]
+        className: "half-size hover-effect",
+        style: { "display": "flex", "flex": "0 1 auto", "flexFlow": "row nowrap", "backgroundColor": randomColor(), "width":"50%", "height":"50%", },
+        items: []
       },
-      {
-        id: v4(),
-        className: "flex half-size hover-effect",
-        style: { backgroundColor: randomColor() },
-        items: [
-          {
-            id: v4(),
-            className: "flex half-size hover-effect",
-            style: { backgroundColor: randomColor() },
-            items: []
-          },
-          {
-            id: v4(),
-            className: "flex half-size hover-effect",
-            style: { backgroundColor: randomColor() },
-            items: []
-          }
-        ]
-      }
-    ]
+      items: [
+        {
+          id: v4(),
+          className: "half-size hover-effect",
+          style: { "display": "flex", "flex": "0 1 auto", "flexFlow": "row nowrap", "backgroundColor": randomColor(), "width":"50%", "height":"50%", },
+          items: [
+            {
+              id: v4(),
+              className: "half-size hover-effect",
+              style: { "display": "flex", "flex": "0 1 auto", "flexFlow": "row nowrap", "backgroundColor": randomColor(), "width":"50%", "height":"50%", },
+              items: [
+                {
+                  id: v4(),
+                  className: "half-size hover-effect",
+                  style: { "display": "flex", "flex": "0 1 auto", "flexFlow": "row nowrap", "backgroundColor": randomColor(), "width":"50%", "height":"50%", },
+                  items: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: v4(),
+          className: "half-size hover-effect",
+          style: { "display": "flex", "flex": "0 1 auto", "flexFlow": "row nowrap", "backgroundColor": randomColor(), "width":"50%", "height":"50%", },
+          items: [
+            {
+              id: v4(),
+              className: "half-size hover-effect",
+              style: { "display": "flex", "flex": "0 1 auto", "flexFlow": "row nowrap", "backgroundColor": randomColor(), "width":"50%", "height":"50%", },
+              items: []
+            },
+            {
+              id: v4(),
+              className: "half-size hover-effect",
+              style: { "display": "flex", "flex": "0 1 auto", "flexFlow": "row nowrap", "backgroundColor": randomColor(), "width":"50%", "height":"50%", },
+              items: []
+            }
+          ]
+        }
+      ]
+    };
+    const store = createStore(editorReducer, persistedState);
+    return store;
   };
-  const store = createStore(editorReducer, persistedState);
-  return store;
-};
 
-export default configureStore;
+  export default configureStore;
